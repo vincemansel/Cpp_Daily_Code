b25a14c204563ade71fa8cde16869b063635b063
Vince Mansel <vincemansel@gmail.com>
2011-10-16 16:32:10 -0700

    Boggle: Finished and fixed #3

--- Programs/boggle.cpp
+++ Programs/boggle.cpp
@@ -15,13 +15,22 @@
 #include "gboggle.h"
 #include "lexicon.h"
 #include "map.h"
+#include "set.h"
 
 struct cubeT {
     int num;
     char letter;
 };
 
+struct pointT {
+    int row;
+    int col;
+    pointT * next;
+};
+
 const int MinLengthWord = 4;
+const double HighlightSpeedComputer = 0.01;
+const double HighlightSpeedHuman = 0.25;
 
 string StandardCubes[16]  = 
 {"AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS", "AOOTTW", "CIMOTU", "DEILRX", "DELRVY",
@@ -59,69 +68,84 @@ static void Welcome()
 
 void InitBoard(Grid<cubeT> &board);
 void Shake(Grid<cubeT> &board);
-void HumanTurn(Grid<cubeT> & board, Lexicon & lex, Map<string> &wordList);
+//void HumanTurn(Grid<cubeT> & board, Lexicon & lex, Map<string> &wordList);
+void HumanTurn(Grid<cubeT> & board, Lexicon & lex, Set<string> &wordSet);
 bool OnBoardHuman(Grid<cubeT> & board, string word);
-void ComputerTurn(Grid<cubeT> & board, Lexicon & lex, Map<string> &wordList);
+//void ComputerTurn(Grid<cubeT> & board, Lexicon & lex, Map<string> &wordList);
+void ComputerTurn(Grid<cubeT> & board, Lexicon & lex, Set<string> &wordSet);
+void PrintString(string word);
 void PrintString(string key, string word);
+//Map<string> DivideLists(Map<string> wordList1, Map<string> wordList2);
+
+pointT * ExtendTracker(pointT *  tracker, int row, int col);
+bool RecursiveHuman(Grid<cubeT> & board, string soFar, string rest, int row, int col, pointT * tracker);
+void RecursiveComputer(Grid<cubeT> & board, Lexicon &lex, Set<string> &wordSet, string word, int row, int col, pointT * tracker);
 
 int main()
 {
     Randomize();
-	SetWindowSize(9, 5);
-	InitGraphics();
+	SetWindowSize(9, 10);
+	
 	Welcome();
-	//GiveInstructions();
+	GiveInstructions();
     
-    int numRows = 4;
-    int numCols = 4;
-    
-    Grid<cubeT> board(numRows, numCols);
     Lexicon lex("lexicon.dat");
-    Map<string> wordList;
     
-    DrawBoard(numRows, numCols);
-    InitBoard(board);
-    Shake(board);
-    
-    // TEST CODE
-    
-    int k = 0;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            board[i][j].letter = IntegerToString(k)[0];
-            LabelCube(i, j, IntegerToString(k++)[0]);
-            if (k > 9) k = 0;
+    while (true) {
+        int numRows;
+        int numCols;
+        
+        while (true) {
+            cout << "Which Boggle would you like to play (Big, Little)?: ";
+            string line = GetLine();
+            line = ConvertToLowerCase(line);
+            if (line == "big" || line == "b") {
+                numRows = 5;
+                numCols = 5;
+                break;
+            }
+            else if (line == "little" || line == "l") {
+                numRows = 4;
+                numCols = 4;
+                break;
+            }
+            else {
+                cout << "Try again... " << endl;
+            }
         }
+        
+        InitGraphics();
+        Grid<cubeT> board(numRows, numCols);
+        Set<string> wordSet;
+        
+        DrawBoard(numRows, numCols);
+        InitBoard(board);
+        Shake(board);
+        
+        HumanTurn(board, lex, wordSet);   
+        
+        //    Set<string> wordSet1 = wordSet;
+        ComputerTurn(board, lex, wordSet);
+        
+        //    cout << "Combined List" << endl;
+        //    wordSet.mapAll(PrintString);
+        //    
+        //    cout << endl << "Human List" << endl;
+        //    wordSet1.mapAll(PrintString);
+        //
+        //    wordSet.subtract(wordSet1);
+        //    cout << endl << "Computer List" << endl;
+        //    wordSet.mapAll(PrintString);
+        
+        cout << "Would you like to play again? (y,n): ";
+        if (GetLine()[0] == 'n') break;
     }
     
-//    board[1][3].letter = 'T';
-//    board[0][2].letter = 'A';
-//    board[0][1].letter = 'I';
-//    board[0][3].letter = 'N';
-//    LabelCube(1, 3, 'T');
-//    LabelCube(0, 2, 'A');
-//    LabelCube(0, 1, 'I');
-//    LabelCube(0, 3, 'N');
-    
-    board[0][0].letter = 'T';
-    board[0][1].letter = 'A';
-    board[0][2].letter = 'I';
-    board[0][3].letter = 'N';
-    LabelCube(0, 0, 'T');
-    LabelCube(0, 1, 'A');
-    LabelCube(0, 2, 'I');
-    LabelCube(0, 3, 'N');
-
-    ComputerTurn(board, lex, wordList);
-    wordList.mapAll(PrintString);
-    
-    HumanTurn(board, lex, wordList);
-    wordList.mapAll(PrintString);
 	return 0;
 }
 
-void PrintString(string key, string word) {
-    cout << key << endl;
+void PrintString(string word) {
+    cout << word << endl;
 }
 
 void InitBoard(Grid<cubeT> &board) {
@@ -149,16 +173,19 @@ void Shake(Grid<cubeT> &board) {
     }
 }
 
-void HumanTurn(Grid<cubeT> &board, Lexicon &lex, Map<string> &wordList) {
+void HumanTurn(Grid<cubeT> &board, Lexicon &lex, Set<string> &wordSet) {
     while (true) {
         cout << "Enter a word: ";
         string word = GetLine();
-        if (word == "") break;
+        if (word == "") {
+            cout << "Are you sure? (y or n): ";
+            if (GetLine()[0] == 'y') break;
+        }
         word = ConvertToUpperCase(word);
         if (word.length() < MinLengthWord) {
             cout << "Word must have " << MinLengthWord << " or more characters." << endl;
         }
-        else if (wordList.containsKey(word)) {
+        else if (wordSet.contains(word)) {
             cout << word + " has already played. Try again..." << endl;
         }
         else if (!lex.containsWord(word)) {
@@ -168,17 +195,11 @@ void HumanTurn(Grid<cubeT> &board, Lexicon &lex, Map<string> &wordList) {
             cout << word + " can not be spelled with cubes. Try again..." << endl;
         }
         else {
-            wordList.put(word,word);
+            wordSet.add(word);
             RecordWordForPlayer(word, Human);
         }
     }
 }
-
-struct pointT {
-    int row;
-    int col;
-    pointT * next;
-};
 
 bool OffGrid(Grid<cubeT> & board, int row, int col) {
     return row < 0 || col < 0 || row >= board.numRows() ||
@@ -187,7 +208,7 @@ bool OffGrid(Grid<cubeT> & board, int row, int col) {
 
 bool HasVisited(int row, int col, pointT * tracker) {
     
-    if (tracker->next != NULL) {
+    if (tracker != NULL && tracker->next != NULL) {
         if (tracker->next->row == row && tracker->next->col == col) {
             return true;
         }
@@ -198,22 +219,6 @@ bool HasVisited(int row, int col, pointT * tracker) {
     return false;
 }
 
-bool HasVisitedComputer(int row, int col, pointT * tracker) {
-    
-    if (tracker != NULL) {
-        if (tracker->row == row && tracker->col == col) {
-            cout << "   HVC: r:c=" << row << ":" << col << " already visited." << endl;
-            return true;
-        }
-        else {
-            return HasVisitedComputer(row, col, tracker->next);
-        }
-    }
-    cout << "   HVC: r:c=" << row << ":" << col << " not visited." << endl;
-    return false;
-}
-
-
 void PrintLinkedList(pointT * list) {
     if (list == NULL) {
         return;
@@ -222,78 +227,54 @@ void PrintLinkedList(pointT * list) {
     PrintLinkedList(list->next);
 }
 
-void ReVisitAndHightlight( pointT * tracker ) {
+void ReVisitAndHightlight(pointT * tracker, playerT player) {
+    double speed = HighlightSpeedComputer;
+    if (player == Human) {
+        speed = HighlightSpeedHuman;
+    }
     if (tracker != NULL) {
-        ReVisitAndHightlight(tracker->next);
+        ReVisitAndHightlight(tracker->next, player);
         HighlightCube(tracker->row, tracker->col, true);
-        Pause(0.25);
+        Pause(speed);
         HighlightCube(tracker->row, tracker->col, false);
-        //Pause(0.25);
     }
 }
 
-void RecursiveScan(Grid<cubeT> & board, Lexicon &lex, Map<string> &wordList, string word, int row, int col, pointT * tracker);
-
-void ComputerTurn(Grid<cubeT> &board, Lexicon &lex, Map<string> &wordList) {
+void ComputerTurn(Grid<cubeT> &board, Lexicon &lex, Set<string> &wordSet) {
     
     for (int i = 0; i < board.numRows(); i++) {
         for (int j = 0; j < board.numCols(); j++) {
             string word;
             word += board[i][j].letter; 
-            pointT * tracker = new pointT;
-            tracker->row = i;
-            tracker->col = j;
-            tracker->next = NULL;
-            RecursiveScan(board, lex, wordList, word, i, j, tracker);
+            pointT * tracker = ExtendTracker(NULL, i, j);
+            RecursiveComputer(board, lex, wordSet, word, i, j, tracker);
         }
     }
 }
 
-void RecursiveScan(Grid<cubeT> & board, Lexicon &lex, Map<string> &wordList, string word, int row, int col, pointT * tracker) {
+void RecursiveComputer(Grid<cubeT> & board, Lexicon &lex, Set<string> &wordSet, string word, int row, int col, pointT * tracker) {
 
-    //PrintLinkedList(tracker);
-    
-    if (word.length() >= MinLengthWord && lex.containsWord(word) && !wordList.containsKey(word)) {  // BASE CASE: SUCCESS
-        //PrintLinkedList(tracker);
-        ReVisitAndHightlight(tracker);
-        wordList.put(word,word);
+    if (word.length() >= MinLengthWord && lex.containsWord(word) && !wordSet.contains(word)) { // BASE CASE: SUCCESS
+        wordSet.add(word);
+        ReVisitAndHightlight(tracker, Computer);
         RecordWordForPlayer(word, Computer);
-        cout << "---------------------- R4: WORD FOUND FOUND:" << word << " r:c=" << row << ":" << col << endl << endl;
+        // Don't return yet: allows for search for plurals, compound words and extended words
     }
     
     for (int dx = -1; dx <= 1; dx++) {
         for (int dy = -1; dy <= 1; dy++) {
             if (!(dx == 0 && dy == 0) && !OffGrid(board,row+dx,col+dy)) {
-                cout << "R1: " << word << " r:c=" << row << ":" << col << " dx:dy=" << dx << ":" << dy << endl;
-                if (lex.containsPrefix(word)) { // BASE CASE: FAIL FAST
-                    cout << "  >>> TrackerList <<<" << endl;
-                    PrintLinkedList(tracker);
-                    cout << "Next: r+dx:c+dy=" << row+dx << ":" << col+dy << endl;
-                    cout << "***********************" << endl;
-                    if (word.length() > 1 && HasVisitedComputer(row+dx, col+dy, tracker)) {// BASE CASE: FAILED
-                        cout << " R2: " << word << " r:c=" << row << ":" << col << " dx:dy=" << dx << ":" << dy << endl << endl << endl;
-                        return;
-                    }
-                    else {
-                        word += board[row+dx][col+dy].letter;
-                        if (lex.containsPrefix(word)) { // BASE CASE: FAIL FAST
-                            cout << "  R3: NEW WORD to search:" << word << " r:c=" << row << ":" << col << " dx:dy=" << dx << ":" << dy << endl;
-                            
-                            pointT * tracker2 = new pointT;
-                            tracker2->next = tracker;       // tracker2 is now the listHead point back up the call chain
-                            tracker2->row = row+dx;
-                            tracker2->col = col+dy;
-                            
-                            RecursiveScan(board, lex, wordList, word, row+dx, col+dy, tracker2);
-                        }
-                    }
-                }
+                string tempWord = word + board[row+dx][col+dy].letter;  // This is key! tempWord does not allow mispellings.
+                pointT * tracker2 = ExtendTracker(tracker, row+dx, col+dy);
+                if (HasVisited(row+dx, col+dy, tracker2)) // BASE CASE: FAILED
+                    continue;
+                if (!lex.containsPrefix(tempWord)) // BASE CASE: FAILED
+                    continue;
+                RecursiveComputer(board, lex, wordSet, tempWord, row+dx, col+dy, tracker2);
             }
         }
-    } 
+    }
 }
-
-bool RecursiveOnBoard(Grid<cubeT> & board, string soFar, string rest, int row, int col, pointT * tracker);
 
 bool OnBoardHuman(Grid<cubeT> & board, string word) {
     string soFar;
@@ -301,29 +282,21 @@ bool OnBoardHuman(Grid<cubeT> & board, string word) {
     for (int i = 0; i < board.numRows(); i++) {
         for (int j = 0; j < board.numCols(); j++) {
             if (board[i][j].letter == word[0]) {
-                pointT * tracker = new pointT;
-                tracker->row = i;
-                tracker->col = j;
-                tracker->next = NULL;
-                
+                pointT * tracker = ExtendTracker(NULL, i, j);
                 soFar += word[0];
-                
-                if (RecursiveOnBoard(board, soFar, word.substr(1), i, j, tracker)) return true;
+                if (RecursiveHuman(board, soFar, word.substr(1), i, j, tracker)) return true;
             }
         }
     }
     return false;
 }
 
-bool RecursiveOnBoard(Grid<cubeT> & board, string soFar, string rest, int row, int col, pointT * tracker) {
-    
-    //PrintLinkedList(tracker);
+bool RecursiveHuman(Grid<cubeT> & board, string soFar, string rest, int row, int col, pointT * tracker) {    
     if (HasVisited(row, col, tracker)) // BASE CASE: FAILED
         return false;
     
     if (rest.length() == 0) { // BASE CASE: SUCCESS
-        //PrintLinkedList(tracker);
-        ReVisitAndHightlight(tracker);
+        ReVisitAndHightlight(tracker, Human);
         return true;
     }
     
@@ -331,14 +304,9 @@ bool RecursiveOnBoard(Grid<cubeT> & board, string soFar, string rest, int row, i
         for (int dy = -1; dy <= 1; dy++) {
             if (!(dx == 0 && dy == 0) && !OffGrid(board,row+dx,col+dy)) {
                 if (board[row+dx][col+dy].letter == rest[0]) {
-                    pointT * tracker2 = new pointT;
-                    tracker2->next = tracker;       // tracker2 is now the listHead point back up the call chain
-                    tracker2->row = row+dx;
-                    tracker2->col = col+dy;
-                    
+                    pointT * tracker2 = ExtendTracker(tracker, row+dx, col+dy);
                     soFar += rest[0];
-                    
-                    if (RecursiveOnBoard(board, soFar, rest.substr(1), row+dx, col+dy, tracker2))
+                    if (RecursiveHuman(board, soFar, rest.substr(1), row+dx, col+dy, tracker2))
                         return true;
                 }
             }
@@ -347,5 +315,96 @@ bool RecursiveOnBoard(Grid<cubeT> & board, string soFar, string rest, int row, i
     return false;
 }
 
+pointT * ExtendTracker(pointT *  tracker, int row, int col) {
+    pointT * tracker2 = new pointT;
+    tracker2->next = tracker;       // tracker2 is now the listHead and points back up the call chain
+    tracker2->row = row;
+    tracker2->col = col;
+    return tracker2;
+}
 
+// TODO: Delete these utility functions when wordSet implemented.
+Set<string> MapToSet(Map<string> wordList) {
+    Map<string>::Iterator itr = wordList.iterator();
+    Set<string> wordSet;
+    
+    while(itr.hasNext())
+        wordSet.add(itr.next());
+    
+    return wordSet;
+}
+
+Map<string> SetToMap(Set<string> wordSet1) {
+    
+    Set<string>::Iterator itr = wordSet1.iterator();
+    Map<string> wordList;
+    
+    while(itr.hasNext()) {
+        string word = itr.next();
+        wordList.add(word, word);
+    }
+    
+    return wordList; 
+}
+
+Map<string> DivideLists(Map<string> wordList1, Map<string> wordList2) {
+    Set<string> wordSet1 = MapToSet(wordList1);
+    Set<string> wordSet2 = MapToSet(wordList2);
+    
+    wordSet1.subtract(wordSet2);
+    
+    return SetToMap(wordSet1);
+}
+
+void PrintString(string key, string word) {
+    cout << key << endl;
+}
+
+void testOnly() {
+// TEST CODE
+
+//    int k = 0;
+//    for (int i = 0; i < 4; i++) {
+//        for (int j = 0; j < 4; j++) {
+//            board[i][j].letter = IntegerToString(k)[0];
+//            LabelCube(i, j, IntegerToString(k++)[0]);
+//            if (k > 9) k = 0;
+//        }
+//    }
+
+//    board[1][3].letter = 'F';
+//    board[0][2].letter = 'O';
+//    board[0][1].letter = 'U';
+//    board[0][3].letter = 'R';
+//    LabelCube(1, 3, 'F');
+//    LabelCube(0, 2, 'O');
+//    LabelCube(0, 1, 'U');
+//    LabelCube(0, 3, 'R');
+
+//    board[0][0].letter = 'F';
+//    board[0][1].letter = 'O';
+//    board[0][2].letter = 'U';
+//    board[0][3].letter = 'R';
+//    LabelCube(0, 0, 'F');
+//    LabelCube(0, 1, 'O');
+//    LabelCube(0, 2, 'U');
+//    LabelCube(0, 3, 'R');
+
+//    board[1][1].letter = 'N';
+//    board[2][1].letter = 'E';
+//    board[3][2].letter = 'A';
+//    board[3][3].letter = 'R';
+//    board[2][2].letter = 'E';
+//    board[1][2].letter = 'S';
+//    board[0][3].letter = 'T';
+
+//    LabelCube(1, 1, 'N');
+//    LabelCube(2, 1, 'E');
+//    LabelCube(3, 2, 'A');
+//    LabelCube(3, 3, 'R');
+//    LabelCube(2, 2, 'E');
+//    LabelCube(1, 2, 'S');
+//    LabelCube(0, 3, 'T');
+    
+}
 
